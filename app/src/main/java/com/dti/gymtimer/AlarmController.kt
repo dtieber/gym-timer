8e68016736e8b25991c527445ead0b1e9209c40d
Daniel Tieber <daniel.tieber@dynatrace.com>
2025-10-11 14:06:15 +0200

    feat: remove auto stop

--- app/src/main/java/com/dti/gymtimer/AlarmController.kt
+++ app/src/main/java/com/dti/gymtimer/AlarmController.kt
@@ -1,11 +1,6 @@
 package com.dti.gymtimer
 
 import android.content.Context
-import android.util.Log
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 private const val TAG = "GymTimer-Alarm"
 
@@ -15,7 +10,6 @@ class AlarmController {
 
         fun getInstance(): AlarmController? = instance
     }
-    private var autoStopJob: Job? = null
 
     private var notificationService: NotificationService = NotificationService()
 
@@ -25,21 +19,13 @@ class AlarmController {
         instance = this
     }
 
-    fun start(context: Context, scope: CoroutineScope, onShow: () -> Unit, onHide: () -> Unit) {
+    fun start(context: Context) {
         vibrate(context)
-        onShow()
         notificationService.showStopNotification(context)
         soundService.playSound(context)
-        autoStopJob = scope.launch {
-            delay(10000L)
-            stop(context)
-            Log.d(TAG, "Alarm auto stopped")
-            onHide()
-        }
     }
 
     fun stop(context: Context) {
-        autoStopJob?.cancel()
         soundService.stopSound(context)
         notificationService.cancelNotification(context)
     }
