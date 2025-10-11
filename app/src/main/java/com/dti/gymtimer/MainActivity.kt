6a52b99e2c085c76b20a072e678f4495e212ab69
Daniel Tieber <daniel.tieber@dynatrace.com>
2025-10-11 16:36:19 +0200

    feat: request battery optimization exemption

--- app/src/main/java/com/dti/gymtimer/MainActivity.kt
+++ app/src/main/java/com/dti/gymtimer/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.compose.setContent
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestBatteryOptimizationExemption(this)
         setContent { GymTimerApp(this) }
     }
 }
