2cae9917fcaf907e5a84a8147e77fcc951b2991f
Daniel Tieber <daniel.tieber@dynatrace.com>
2025-10-12 13:48:37 +0200

    feat: add log messages to alarm controller

--- app/src/main/java/com/dti/gymtimer/AlarmController.kt
+++ app/src/main/java/com/dti/gymtimer/AlarmController.kt
@@ -1,6 +1,7 @@
 package com.dti.gymtimer
 
 import android.content.Context
+import android.util.Log
 
 private const val TAG = "GymTimer-Alarm"
 
@@ -20,12 +21,14 @@ class AlarmController {
     }
 
     fun start(context: Context) {
+        Log.d(TAG, "Starting alarm")
         vibrate(context)
         notificationService.showStopNotification(context)
         soundService.playSound(context)
     }
 
     fun stop(context: Context) {
+        Log.d(TAG, "Stopping alarm")
         soundService.stopSound(context)
         notificationService.cancelNotification(context)
     }
