3cc4248ed82c088cf446da3ee090a3c1c9bc7216
Daniel Tieber <daniel.tieber@dynatrace.com>
2025-10-12 13:52:38 +0200

    feat: add option to toggle countdown

--- app/src/main/java/com/dti/gymtimer/CountdownService.kt
+++ app/src/main/java/com/dti/gymtimer/CountdownService.kt
@@ -6,15 +6,28 @@ import kotlinx.coroutines.flow.flow
 
 class CountdownService {
 
+    private var isPaused = false
     private var remainingSeconds = 0
 
     fun startCountdown(initialSeconds: Int): Flow<CountdownEvent> = flow {
         remainingSeconds = initialSeconds
+        isPaused = false
 
         while (remainingSeconds >= 0) {
-            emit(CountdownEvent.CountdownUpdated(remainingSeconds))
+            if (!isPaused) {
+                emit(CountdownEvent.CountdownUpdated(remainingSeconds))
+                remainingSeconds--
+            }
             delay(1000L)
         }
         emit(CountdownEvent.CountdownCompleted)
+    }
+
+    fun togglePause() {
+        isPaused = !isPaused
+    }
+
+    fun addSeconds(seconds: Int) {
+        remainingSeconds += seconds
     }
 }
